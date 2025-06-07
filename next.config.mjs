76b76dbe794d476390150cba716063d12dc3da7b
manishaa15-07 <manishachoudhary244365@gmail.com>
2025-06-07 19:21:44 +0530

    made-dashboard and integrated backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['randomuser.me'],
+    },
+};
+
 
 export default nextConfig;
+
+
+
+
